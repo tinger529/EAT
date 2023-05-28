b314dbb457810440eee348239897092e5fd47686
Elan Lin <elan00001@gmail.com>
2023-05-28 15:59:40 +0800

    change to axios

--- eat-frontend/src/api/Django.jsx
+++ eat-frontend/src/api/Django.jsx
@@ -1,4 +1,5 @@
 //TODO: use rest api to communicate with django backend
+import axios from 'axios';
 
 let api = {
 
@@ -6,50 +7,23 @@ let api = {
     provider: () => {
 
     },
+    
+    baseURL: 'http://localhost:8000/api', // Replace with your Django API URL
 
     createAccount: async (email, password, name) => {
-        // use http rest api to communicate with django backend
-        //end point: /api/v1/account
-        //method: POST
-        //return: user
-        //response: 201
-        //error: 400
-        //TODO: implement this function
-
-        const endpoint = '/api/v1/account';
-        const method = 'POST';
+        const data = {
+            email,
+            password,
+            name,
+        };
 
         try {
-            const response = await fetch(endpoint, {
-                method: method,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    email: email,
-                    password: password,
-                    name: name,
-                }),
-            });
-
-            if (response.status === 201) {
-                // Return the session if the account is created successfully
-                const data = await response.json();
-                return data.user;
-            } else if (response.status === 400) {
-                // Handle error if the request is bad
-                throw new Error('Bad Request');
-            } else {
-                // Handle any other errors
-                throw new Error(`Unexpected error: ${response.status}`);
-            }
+            const response = await axios.post(`${api.baseURL}/accounts/create`, data);
+            return response.data;
         } catch (error) {
-            console.error('Error in createAccount:', error);
-            throw error;
+            throw new Error(error.response.data.error); // Adjust the error handling as needed
         }
-
     },
-
 
     getAccount: async () => {
         // get login user's account information
@@ -58,33 +32,12 @@ let api = {
         // return: user
         // response: 200
         // error: 401
-        const endpoint = '/api/v1/account';
-        const method = 'GET';
-
         try {
-            const response = await fetch(endpoint, {
-                method: method,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-
-            if (response.status === 200) {
-                // Return the session if the account is created successfully
-                const data = await response.json();
-                return data.user;
-            } else if (response.status === 401) {
-                // Handle error if the request is bad
-                throw new Error('Unauthorized');
-            } else {
-                // Handle any other errors
-                throw new Error(`Unexpected error: ${response.status}`);
-            }
+            const response = await axios.get(`${api.baseURL}/accounts`);
+            return response.data;
         } catch (error) {
-            console.error('Error in getAccount:', error);
-            throw error;
+            throw new Error(error.response.data.error); // Adjust the error handling as needed
         }
-
     },
 
     getuserinformation: async (userid) => {
@@ -132,38 +85,17 @@ let api = {
         // response: 201
         // error: 400
         //create a session for the user
-        const endpoint = '/api/v1/account/sessions/email';
-        const method = 'POST';
+        const data = {
+            email,
+            password,
+        };
 
         try {
-            const response = await fetch(endpoint, {
-                method: method,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    email: email,
-                    password: password,
-                }),
-            });
-
-            if (response.status === 201) {
-                // Return the session if the account is created successfully
-                const data = await response.json();
-                return data.session;
-            } else if (response.status === 400) {
-                // Handle error if the request is bad
-                throw new Error('Bad Request');
-            } else {
-                // Handle any other errors
-                throw new Error(`Unexpected error: ${response.status}`);
-            }
+            const response = await axios.post(`${api.baseURL}/sessions/create`, data);
+            return response.data;
         } catch (error) {
-            console.error('Error in createSession:', error);
-            throw error;
+            throw new Error(error.response.data.error); // Adjust the error handling as needed
         }
-
-
     },
 
     deleteCurrentSession: async () => {
@@ -173,39 +105,67 @@ let api = {
         // response: 204
         // error: 401
         //delete the current session
-
-        const endpoint = '/api/v1/account/sessions/{sessionId}';
-        const method = 'DELETE';
-
-        return fetch(endpoint, {
-            method: method,
-        })
-            .then(res => {
-                if (res.status === 204) {
-                    return
-                } else {
-                    throw Error('Unauthorized')
-                }
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    },
-    createGroup: (groupName) => {
-
+        try {
+            const response = await axios.delete(`${api.baseURL}/sessions/current`);
+            return response.data;
+        } catch (error) {
+            throw new Error(error.response.data.error); // Adjust the error handling as needed
+        }
     },
 
-    listGroups: () => {
+    //to be done :create groupid
+    createGroup: async (groupName) => {
+        const data = {
+            name: groupName,
+        };
 
+        try {
+            const response = await axios.post(`${api.baseURL}/groups/create`, data);
+            return response.data;
+        } catch (error) {
+            throw new Error(error.response.data.error); // Adjust the error handling as needed
+        }
     },
 
+    listGroups: async () => {
+        try {
+            const response = await axios.get(`${api.baseURL}/groups`);
+            return response.data;
+        } catch (error) {
+            throw new Error(error.response.data.error); // Adjust the error handling as needed
+        }
+    },
 
-    //return group object
-    getGroupInfo: (groupId) => {
+    getGroup: async (groupId) => {
+        try {
+            const response = await axios.get(`${api.baseURL}/groups/${groupId}`);
+            return response.data;
+        } catch (error) {
+            throw new Error(error.response.data.error); // Adjust the error handling as needed
+        }
+    },
 
+    listGroupMemberships: async (groupId) => {
+        try {
+            const response = await axios.get(`${api.baseURL}/groups/${groupId}/memberships`);
+            return response.data;
+        } catch (error) {
+            throw new Error(error.response.data.error); // Adjust the error handling as needed
+        }
+    },
+
+    getGroupInfo: async (groupId) => {
+        try {
+            const membershipResponse = await api.listGroupMemberships(groupId);
+            const groupResponse = await api.getGroup(groupId);
+            return { $id: groupId, members: membershipResponse.memberships, name: groupResponse.name };
+        } catch (error) {
+            throw new Error(error.response.data.error); // Adjust the error handling as needed
+        }
     },
 
 
 };
+
 
 export default api;
